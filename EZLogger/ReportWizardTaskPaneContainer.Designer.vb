edbae4477826f51d0849886d4107ac778dbfb86b
lundeen-bryan <lundeen.bryan@gmail.com>
2025-03-27 21:01:19 -0700

    Enhance Report Wizard with new UI and dependencies
    Updated `ReportWizardPanel.xaml` to include a new layout with input fields for patient information and a database view button.
    
    Modified `EZLogger.vbproj` to add and update NuGet package references, reorganizing project structure into `Controls` and `Views` folders.
    
    Introduced `ILLink.Descriptors.LibraryBuild.xml` for linker settings and created a new `MainVM.vb` for data binding.
    
    Updated `app.config` with assembly binding redirects and generated `packages.config` for NuGet dependencies.
    
    Added `Notes.txt` for documentation on using Haley library converters.
    
    Adjusted `ReportWizardTaskPaneContainer.Designer.vb` to integrate the new `ReportWizardPanel` and created `EzLoggerRD.xaml` for shared resources.
    
    Updated `ThisAddIn.vb` to instantiate the new panels in the task pane.

--- EZLogger/ReportWizardTaskPaneContainer.Designer.vb
+++ EZLogger/ReportWizardTaskPaneContainer.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class ReportWizardTaskPaneContainer
     Inherits System.Windows.Forms.UserControl
 
     'UserControl overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,45 +20,34 @@ Partial Class ReportWizardTaskPaneContainer
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.ElementHost1 = New System.Windows.Forms.Integration.ElementHost()
         Me.ReportWizardPanel1 = New EZLogger.ReportWizardPanel()
-        Me.LabelReportWizardHere = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'ElementHost1
         '
+        Me.ElementHost1.BackColor = System.Drawing.SystemColors.Control
         Me.ElementHost1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.ElementHost1.Location = New System.Drawing.Point(0, 0)
         Me.ElementHost1.Name = "ElementHost1"
-        Me.ElementHost1.Size = New System.Drawing.Size(150, 150)
+        Me.ElementHost1.Size = New System.Drawing.Size(302, 319)
         Me.ElementHost1.TabIndex = 0
         Me.ElementHost1.Text = "ElementHost1"
         Me.ElementHost1.Child = Me.ReportWizardPanel1
-        '
-        'LabelReportWizardHere
-        '
-        Me.LabelReportWizardHere.AutoSize = True
-        Me.LabelReportWizardHere.Location = New System.Drawing.Point(26, 55)
-        Me.LabelReportWizardHere.Name = "LabelReportWizardHere"
-        Me.LabelReportWizardHere.Size = New System.Drawing.Size(101, 13)
-        Me.LabelReportWizardHere.TabIndex = 1
-        Me.LabelReportWizardHere.Text = "Report Wizard Here"
         '
         'ReportWizardTaskPaneContainer
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Controls.Add(Me.LabelReportWizardHere)
         Me.Controls.Add(Me.ElementHost1)
         Me.Name = "ReportWizardTaskPaneContainer"
+        Me.Size = New System.Drawing.Size(302, 319)
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ElementHost1 As Windows.Forms.Integration.ElementHost
     Friend ReportWizardPanel1 As ReportWizardPanel
-    Friend WithEvents LabelReportWizardHere As Windows.Forms.Label
 End Class
---
+++ EZLogger/ViewModels/MainVM.vb
@@ -0,0 +1,3 @@
+﻿Public Class MainVM
+
+End Class
