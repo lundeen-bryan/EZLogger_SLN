aa7353c945544d2042e3ea74c3f10987ac0abb57
lundeen-bryan <lundeen.bryan@gmail.com>
2025-03-29 15:04:55 -0700

    Enhance TaskStepControl with dynamic content binding
    Updated TaskStepControl.xaml to bind ButtonContent and CheckBoxContent properties.
    Implemented these dependency properties in TaskStepControl.xaml.vb.
    Modified ReportWizardPanel.xaml to instantiate TaskStepControl with specific content values, improving UI usability.

--- EZLogger/Controls/TaskStepControl.xaml.vb
+++ EZLogger/Controls/TaskStepControl.xaml.vb
@@ -1,3 +1,46 @@
-﻿Public Class TaskStepControl
+﻿Imports System.Windows
+Imports System.Windows.Controls
 
+Public Class TaskStepControl
+    Inherits UserControl
+
+    ' Constructor to load XAML
+    Public Sub New()
+        ' This method is auto-generated and connects the XAML to this code-behind
+        InitializeComponent()
+    End Sub
+
+    ' === ButtonContent Dependency Property ===
+    Public Shared ReadOnly ButtonContentProperty As DependencyProperty =
+        DependencyProperty.Register(
+            "ButtonContent",
+            GetType(String),
+            GetType(TaskStepControl),
+            New PropertyMetadata("Button"))
+
+    Public Property ButtonContent As String
+        Get
+            Return CType(GetValue(ButtonContentProperty), String)
+        End Get
+        Set(value As String)
+            SetValue(ButtonContentProperty, value)
+        End Set
+    End Property
+
+    ' === CheckBoxContent Dependency Property ===
+    Public Shared ReadOnly CheckBoxContentProperty As DependencyProperty =
+        DependencyProperty.Register(
+            "CheckBoxContent",
+            GetType(String),
+            GetType(TaskStepControl),
+            New PropertyMetadata("CheckBox"))
+
+    Public Property CheckBoxContent As String
+        Get
+            Return CType(GetValue(CheckBoxContentProperty), String)
+        End Get
+        Set(value As String)
+            SetValue(CheckBoxContentProperty, value)
+        End Set
+    End Property
 End Class
