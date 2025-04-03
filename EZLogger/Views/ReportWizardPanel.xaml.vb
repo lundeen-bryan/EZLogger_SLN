c949a6c7f93b3cd0cf09d85298d8db6ba0fb7cea
lundeen-bryan <lundeen.bryan@gmail.com>
2025-04-02 18:09:45 -0700

    Refactor ReportWizardPanel UI and logic
    Removed ComboBox and added ConfirmTypeBtn in XAML.
    Introduced ReportWizardPanel_Loaded to initialize report types.
    Updated ConfirmReportTypeButton_Click to handle selected item as String.

--- EZLogger/Views/ReportWizardPanel.xaml.vb
+++ EZLogger/Views/ReportWizardPanel.xaml.vb
@@ -12,17 +12,27 @@ Partial Public Class ReportWizardPanel
     Private Sub FindPatientId_Click(sender As Object, e As RoutedEventArgs)
         handler.OnSearchButtonClick()
     End Sub
+    Private Sub ReportWizardPanel_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        Dim reportTypes As New List(Of String) From {
+        "1370(b)(1)",
+        "UNLIKELY 1370(b)(1)",
+        "1372(a)(1)"
+    }
+
+        ReportTypeCbo.ItemsSource = reportTypes
+    End Sub
 
     Private Sub PatientDatabaseButton_Click(sender As Object, e As RoutedEventArgs)
         dbhandler.OnPatientDatabaseButtonClick()
     End Sub
 
     Private Sub ConfirmReportTypeButton_Click(sender As Object, e As RoutedEventArgs)
-        Dim selectedItem As ComboBoxItem = TryCast(ReportTypeCbo.SelectedItem, ComboBoxItem)
+        'Dim selectedItem As ComboBoxItem = TryCast(ReportTypeCbo.SelectedItem, ComboBoxItem)
+        Dim selectedItem As String = TryCast(ReportTypeCbo.SelectedItem, String)
 
         If selectedItem IsNot Nothing Then
-            Dim reportType As String = selectedItem.Content.ToString()
-            rthandler.OnConfirmReportTypeButtonClick(reportType)
+            'Dim reportType As String = selectedItem.Content.ToString()
+            rthandler.OnConfirmReportTypeButtonClick(selectedItem)
         Else
             MessageBox.Show("Please select a report type first.", "No Selection")
         End If
