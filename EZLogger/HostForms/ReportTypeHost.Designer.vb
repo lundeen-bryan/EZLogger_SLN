aa183d4dfbe7ebf9cc0da18b5f69431219ae56e8
lundeen-bryan <lundeen.bryan@gmail.com>
2025-04-04 07:28:13 -0700

    Increase UI sizes and expand report type options
    Updated the size of the `ElementHost1` control and the `ClientSize` of the `ReportTypeHost` form from 879 to 896 to improve layout. Added new report types ("PPR", "1026.5(b)(1)", "2972", "1026.2(l)", and "1026.2(b)") to the `reportTypes` list in the `ReportWizardPanel_Loaded` method, enhancing the report wizard's functionality.

--- EZLogger/HostForms/ReportTypeHost.Designer.vb
+++ EZLogger/HostForms/ReportTypeHost.Designer.vb
@@ -31,7 +31,7 @@ Partial Class ReportTypeHost
         Me.ElementHost1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.ElementHost1.Location = New System.Drawing.Point(0, 0)
         Me.ElementHost1.Name = "ElementHost1"
-        Me.ElementHost1.Size = New System.Drawing.Size(446, 879)
+        Me.ElementHost1.Size = New System.Drawing.Size(446, 896)
         Me.ElementHost1.TabIndex = 0
         Me.ElementHost1.Text = "ElementHost1"
         Me.ElementHost1.Child = Me.ReportTypeView1
@@ -41,7 +41,7 @@ Partial Class ReportTypeHost
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(446, 879)
+        Me.ClientSize = New System.Drawing.Size(446, 896)
         Me.Controls.Add(Me.ElementHost1)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
