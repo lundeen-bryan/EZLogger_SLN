7b425a41073be6cd7ccd8f0a217691bbdcb4c288
lundeen-bryan <lundeen.bryan@gmail.com>
2025-04-06 10:57:14 -0700

    Add MoveCopy feature with new handlers and views
    This commit introduces a "MoveCopy" feature to the EZLogger project.
    Key changes include the addition of the `MoveCopyHandler` class for managing file operations, along with new views (`MoveCopyView` and `FileMoveView`) for user interaction.
    
    The `ReportWizardPanel` class has been updated to integrate the new functionality, and a modal form class `MoveCopyHost` has been created to host the views.
    
    Additionally, a resource file `MoveCopyHost.resx` has been added for localization support, and the corresponding code-behind files have been implemented to ensure functionality.

---
+++ EZLogger/HostForms/MoveCopyHost.vb
@@ -0,0 +1,3 @@
+﻿Public Class MoveCopyHost
+
+End Class
---
+++ EZLogger/Views/MoveCopyView.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Windows.Controls
+
+Namespace EZLogger.Views
+    Public Class MoveCopyView
+        Inherits UserControl
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+    End Class
+End Namespace
