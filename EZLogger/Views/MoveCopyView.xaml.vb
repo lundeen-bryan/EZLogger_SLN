df4400624e5acdc9591c90a7da3c0f89439ef336
lundeen-bryan <lundeen.bryan@gmail.com>
2025-04-06 13:34:33 -0700

    Enhance MoveCopyView with button click handler
    Added imports for necessary namespaces and changed the class
    declaration to partial. Implemented a new event handler for
    the BtnSearchPatientId button that displays a message box
    when clicked, confirming the action to the user.

--- EZLogger/Views/MoveCopyView.xaml.vb
+++ EZLogger/Views/MoveCopyView.xaml.vb
@@ -1,12 +1,28 @@
-﻿Imports System.Windows.Controls
+﻿Imports System.Windows
+Imports System.Windows.Controls
+Imports EZLogger.Helpers
+Imports EZLogger.Enums
 
 Namespace EZLogger.Views
-    Public Class MoveCopyView
+
+    Partial Public Class MoveCopyView
         Inherits UserControl
 
         Public Sub New()
             InitializeComponent()
+
+            AddHandler BtnSearchPatientId.Click, AddressOf BtnSearchPatientId_Click
+        End Sub
+
+        Private Sub BtnSearchPatientId_Click(sender As Object, e As RoutedEventArgs)
+            Dim config As New MessageBoxConfig With {
+                .Message = "You pressed the search button.",
+                .ShowOk = True
+            }
+
+            CustomMsgBoxHandler.Show(config)
         End Sub
 
     End Class
+
 End Namespace
