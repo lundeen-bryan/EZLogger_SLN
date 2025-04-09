448d5c1fb7511547674b17360e2ee0513d01bf08
lundeen-bryan <lundeen.bryan@gmail.com>
2025-04-08 20:32:08 -0700

    Enhance XML documentation in FormPositionHelper.vb
    Updated `FormPositionHelper.vb` to include detailed XML comments.
    Added remarks about the module's purpose in multi-monitor setups
    and provided an example for the `MoveFormToTopLeftOfAllScreens`
    method to improve clarity and usability for developers.

--- EZLogger/Helpers/FormPositionHelper.vb
+++ EZLogger/Helpers/FormPositionHelper.vb
@@ -4,6 +4,11 @@ Imports System.Windows.Forms
 ''' <summary>
 ''' Provides helper methods to position WinForms forms in a multi-monitor environment.
 ''' </summary>
+''' <remarks>
+''' This module is designed to assist in positioning forms relative to the virtual desktop,
+''' which spans across all connected monitors. It is particularly useful for applications
+''' that need to ensure forms are displayed in consistent locations regardless of the monitor setup.
+''' </remarks>
 Public Module FormPositionHelper
 
     ''' <summary>
@@ -13,6 +18,15 @@ Public Module FormPositionHelper
     ''' <param name="form">The WinForms form to move.</param>
     ''' <param name="leftOffset">Horizontal offset in pixels from the left edge of the virtual desktop.</param>
     ''' <param name="topOffset">Vertical offset in pixels from the top edge of the virtual desktop.</param>
+    ''' <example>
+    ''' Dim myForm As New Form()
+    ''' FormPositionHelper.MoveFormToTopLeftOfAllScreens(myForm, 10, 20)
+    ''' </example>
+    ''' <remarks>
+    ''' This method sets the form's start position to manual and calculates its position
+    ''' based on the virtual screen's top-left corner. It ensures the form is placed
+    ''' correctly even in multi-monitor setups.
+    ''' </remarks>
     Public Sub MoveFormToTopLeftOfAllScreens(form As Form,
                                              Optional leftOffset As Integer = 0,
                                              Optional topOffset As Integer = 0)
