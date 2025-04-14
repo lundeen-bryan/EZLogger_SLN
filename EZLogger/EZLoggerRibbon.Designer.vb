612e91e40b5039498a39b2fce0dc44a0e65e8247
lundeen-bryan <lundeen.bryan@gmail.com>
2025-04-14 16:16:35 -0700

    Update logo resources and ribbon UI components
    - Removed old logo images and replaced with new PNG resources of different sizes.
    - Updated `EZLoggerRibbon.Designer.vb` to add new ribbon buttons and groups while removing obsolete ones.
    - Modified `Resources.Designer.vb` and `Resources.resx` to reflect new logo properties and entries.
    - Changed `AboutView.xaml` to display the new logo.
    - Added a new SVG file for scalable logo graphics.

--- EZLogger/EZLoggerRibbon.Designer.vb
+++ EZLogger/EZLoggerRibbon.Designer.vb
@@ -44,26 +44,26 @@
         Me.Tab1 = Me.Factory.CreateRibbonTab
         Me.WizardGroup = Me.Factory.CreateRibbonGroup
         Me.ReportWizardBox = Me.Factory.CreateRibbonBox
+        Me.ReportWizardButton = Me.Factory.CreateRibbonButton
         Me.DatabaseGroup = Me.Factory.CreateRibbonGroup
         Me.DbBtnBox = Me.Factory.CreateRibbonBox
+        Me.DatabaseMenu = Me.Factory.CreateRibbonMenu
         Me.ToolsGroup = Me.Factory.CreateRibbonGroup
         Me.SaveBtnBox = Me.Factory.CreateRibbonBox
-        Me.PdfBtnBox = Me.Factory.CreateRibbonBox
-        Me.TypoBtnBox = Me.Factory.CreateRibbonBox
-        Me.SetupGroup = Me.Factory.CreateRibbonGroup
-        Me.Button2 = Me.Factory.CreateRibbonButton
-        Me.ReportWizardButton = Me.Factory.CreateRibbonButton
-        Me.DatabaseMenu = Me.Factory.CreateRibbonMenu
         Me.SaveButton = Me.Factory.CreateRibbonButton
         Me.Button1 = Me.Factory.CreateRibbonButton
+        Me.PdfBtnBox = Me.Factory.CreateRibbonBox
         Me.ConvertButton = Me.Factory.CreateRibbonButton
+        Me.TypoBtnBox = Me.Factory.CreateRibbonBox
         Me.TypoButton = Me.Factory.CreateRibbonButton
         Me.EmailButton = Me.Factory.CreateRibbonButton
         Me.BtnCloseDoc = Me.Factory.CreateRibbonButton
+        Me.SetupGroup = Me.Factory.CreateRibbonGroup
         Me.HelpButton = Me.Factory.CreateRibbonButton
         Me.SettingsButton = Me.Factory.CreateRibbonButton
         Me.AboutButton = Me.Factory.CreateRibbonButton
         Me.SyncButton = Me.Factory.CreateRibbonButton
+        Me.Button2 = Me.Factory.CreateRibbonButton
         Me.Tab1.SuspendLayout()
         Me.WizardGroup.SuspendLayout()
         Me.ReportWizardBox.SuspendLayout()
@@ -98,6 +98,14 @@
         Me.ReportWizardBox.Items.Add(Me.ReportWizardButton)
         Me.ReportWizardBox.Name = "ReportWizardBox"
         '
+        'ReportWizardButton
+        '
+        Me.ReportWizardButton.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge
+        Me.ReportWizardButton.Image = Global.EZLogger.My.Resources.Resources.EZLoggerLogo64
+        Me.ReportWizardButton.Label = "Process Report"
+        Me.ReportWizardButton.Name = "ReportWizardButton"
+        Me.ReportWizardButton.ShowImage = True
+        '
         'DatabaseGroup
         '
         Me.DatabaseGroup.Items.Add(Me.DbBtnBox)
@@ -108,6 +116,15 @@
         '
         Me.DbBtnBox.Items.Add(Me.DatabaseMenu)
         Me.DbBtnBox.Name = "DbBtnBox"
+        '
+        'DatabaseMenu
+        '
+        Me.DatabaseMenu.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge
+        Me.DatabaseMenu.Dynamic = True
+        Me.DatabaseMenu.Image = Global.EZLogger.My.Resources.Resources.database
+        Me.DatabaseMenu.Label = "Select a Database"
+        Me.DatabaseMenu.Name = "DatabaseMenu"
+        Me.DatabaseMenu.ShowImage = True
         '
         'ToolsGroup
         '
@@ -125,48 +142,6 @@
         Me.SaveBtnBox.Items.Add(Me.SaveButton)
         Me.SaveBtnBox.Name = "SaveBtnBox"
         '
-        'PdfBtnBox
-        '
-        Me.PdfBtnBox.Items.Add(Me.ConvertButton)
-        Me.PdfBtnBox.Name = "PdfBtnBox"
-        '
-        'TypoBtnBox
-        '
-        Me.TypoBtnBox.Items.Add(Me.TypoButton)
-        Me.TypoBtnBox.Name = "TypoBtnBox"
-        '
-        'SetupGroup
-        '
-        Me.SetupGroup.Items.Add(Me.HelpButton)
-        Me.SetupGroup.Items.Add(Me.SettingsButton)
-        Me.SetupGroup.Items.Add(Me.AboutButton)
-        Me.SetupGroup.Items.Add(Me.SyncButton)
-        Me.SetupGroup.Items.Add(Me.Button2)
-        Me.SetupGroup.Label = "Setup Commands"
-        Me.SetupGroup.Name = "SetupGroup"
-        '
-        'Button2
-        '
-        Me.Button2.Label = "Test Button"
-        Me.Button2.Name = "Button2"
-        '
-        'ReportWizardButton
-        '
-        Me.ReportWizardButton.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge
-        Me.ReportWizardButton.Image = Global.EZLogger.My.Resources.Resources.EZLoggerIcon
-        Me.ReportWizardButton.Label = "Process Report"
-        Me.ReportWizardButton.Name = "ReportWizardButton"
-        Me.ReportWizardButton.ShowImage = True
-        '
-        'DatabaseMenu
-        '
-        Me.DatabaseMenu.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge
-        Me.DatabaseMenu.Dynamic = True
-        Me.DatabaseMenu.Image = Global.EZLogger.My.Resources.Resources.database
-        Me.DatabaseMenu.Label = "Select a Database"
-        Me.DatabaseMenu.Name = "DatabaseMenu"
-        Me.DatabaseMenu.ShowImage = True
-        '
         'SaveButton
         '
         Me.SaveButton.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge
@@ -183,6 +158,11 @@
         Me.Button1.Name = "Button1"
         Me.Button1.ShowImage = True
         '
+        'PdfBtnBox
+        '
+        Me.PdfBtnBox.Items.Add(Me.ConvertButton)
+        Me.PdfBtnBox.Name = "PdfBtnBox"
+        '
         'ConvertButton
         '
         Me.ConvertButton.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge
@@ -190,6 +170,11 @@
         Me.ConvertButton.Label = "Convert To PDF"
         Me.ConvertButton.Name = "ConvertButton"
         Me.ConvertButton.ShowImage = True
+        '
+        'TypoBtnBox
+        '
+        Me.TypoBtnBox.Items.Add(Me.TypoButton)
+        Me.TypoBtnBox.Name = "TypoBtnBox"
         '
         'TypoButton
         '
@@ -214,6 +199,16 @@
         Me.BtnCloseDoc.Label = "Close Document"
         Me.BtnCloseDoc.Name = "BtnCloseDoc"
         Me.BtnCloseDoc.ShowImage = True
+        '
+        'SetupGroup
+        '
+        Me.SetupGroup.Items.Add(Me.HelpButton)
+        Me.SetupGroup.Items.Add(Me.SettingsButton)
+        Me.SetupGroup.Items.Add(Me.AboutButton)
+        Me.SetupGroup.Items.Add(Me.SyncButton)
+        Me.SetupGroup.Items.Add(Me.Button2)
+        Me.SetupGroup.Label = "Setup Commands"
+        Me.SetupGroup.Name = "SetupGroup"
         '
         'HelpButton
         '
@@ -242,6 +237,11 @@
         Me.SyncButton.Label = "Sync With SharePoint"
         Me.SyncButton.Name = "SyncButton"
         Me.SyncButton.ShowImage = True
+        '
+        'Button2
+        '
+        Me.Button2.Label = "Test Button"
+        Me.Button2.Name = "Button2"
         '
         'EZLoggerRibbon
         '
