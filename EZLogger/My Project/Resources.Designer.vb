a1ad6d5f21377944db2f0abf0a1d1d88da2bf11e
lundeen-bryan <lundeen.bryan@gmail.com>
2025-04-14 13:31:10 -0700

    Exchanged out the old logo with a new logo and icon (64x64) set.

--- EZLogger/My Project/Resources.Designer.vb
+++ EZLogger/My Project/Resources.Designer.vb
@@ -103,6 +103,26 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property EZLoggerIcon() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("EZLoggerIcon", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property EZLoggerLogo() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("EZLoggerLogo", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property floppy() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("floppy", resourceCulture)
