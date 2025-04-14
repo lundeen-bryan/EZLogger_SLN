6416076903aef377295732b3bc684e31df99b7ad
Lundeen.Bryan <lundeen.bryan@gmail.com>
2025-04-14 07:53:46 -0700

    Added msgboxhelper to about inplace of windows native msgbox

--- EZLogger/ThisAddIn.vb
+++ EZLogger/ThisAddIn.vb
@@ -2,40 +2,32 @@
 Imports Microsoft.Office.Interop.Word
 
 Public Class ThisAddIn
+Private myTaskPane As Microsoft.Office.Tools.CustomTaskPane
+Public ReadOnly Property ReportWizardTaskPane As Microsoft.Office.Tools.CustomTaskPane
+Get
+Return myTaskPane
+End Get
+End Property
 
-    Private myTaskPane As Microsoft.Office.Tools.CustomTaskPane
+Private Sub ThisAddIn_Startup() Handles Me.Startup
+    ' Create an instance of your container UserControl (TaskPaneContainer)
+    Dim myTaskPaneContainer As New ReportWizardTaskPaneContainer()
+    ' Set the dock style For the ElementHost inside the container
+    myTaskPaneContainer.ElementHost1.Dock = System.Windows.Forms.DockStyle.Fill
+    ' Create an instance of your ReportWizardPanel (WPF control)
+    Dim myReportWizardPanel As New ReportWizardPanel()
+    ' Set the ReportWizardPanel As the child of the ElementHost
+    myTaskPaneContainer.ElementHost1.Child = myReportWizardPanel
+    ' Add the container To the CustomTaskPanes collection With a title
+    myTaskPane = Me.CustomTaskPanes.Add(myTaskPaneContainer, "EZ Logger Report Wizard")
+    ' Set the initial size of the task pane
+    myTaskPane.Width = 440
+    ' Initially, keep the panel hidden
+    myTaskPane.Visible = False
+End Sub
 
-    Public ReadOnly Property ReportWizardTaskPane As Microsoft.Office.Tools.CustomTaskPane
-        Get
-            Return myTaskPane
-        End Get
-    End Property
-
-    Private Sub ThisAddIn_Startup() Handles Me.Startup
-        ' Create an instance of your container UserControl (TaskPaneContainer)
-        Dim myTaskPaneContainer As New ReportWizardTaskPaneContainer()
-
-        ' Set the dock style for the ElementHost inside the container
-        myTaskPaneContainer.ElementHost1.Dock = System.Windows.Forms.DockStyle.Fill
-
-        ' Create an instance of your ReportWizardPanel (WPF control)
-        Dim myReportWizardPanel As New ReportWizardPanel()
-
-        ' Set the ReportWizardPanel as the child of the ElementHost
-        myTaskPaneContainer.ElementHost1.Child = myReportWizardPanel
-
-        ' Add the container to the CustomTaskPanes collection with a title
-        myTaskPane = Me.CustomTaskPanes.Add(myTaskPaneContainer, "EZ Logger Report Wizard")
-
-        ' Set the initial size of the task pane
-        myTaskPane.Width = 440
-
-        ' Initially, keep the panel hidden
-        myTaskPane.Visible = False
-    End Sub
-
-    Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
-        ' Any necessary cleanup can be done here
-    End Sub
+Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
+    ' Any necessary cleanup can be done here
+End Sub
 
 End Class
--- EZLogger/Views/AboutView.xaml.vb
+++ EZLogger/Views/AboutView.xaml.vb
@@ -8,49 +8,48 @@ Imports System.Windows.Media.Imaging
 
 Namespace EZLogger.Views
 
-    Partial Public Class AboutView
-        Inherits Windows.Controls.UserControl
+Partial Public Class AboutView
+Inherits Windows.Controls.UserControl
 
-        Private ReadOnly _hostForm As Form
-        Private ReadOnly configFilePath As String = ConfigPathHelper.GetGlobalConfigPath()
+Private ReadOnly _hostForm As Form
+Private ReadOnly configFilePath As String = ConfigPathHelper.GetGlobalConfigPath()
 
-        Public Sub New(Optional hostForm As Form = Nothing)
-            InitializeComponent()
-            _hostForm = hostForm
-            LoadAboutInfo()
+Public Sub New(Optional hostForm As Form = Nothing)
+    InitializeComponent()
+    _hostForm = hostForm
+    LoadAboutInfo()
+    AddHandler BtnHelp.Click, AddressOf BtnHelp_Click
+    AddHandler BtnGoBack.Click, AddressOf BtnGoBack_Click
+End Sub
 
-            AddHandler BtnHelp.Click, AddressOf BtnHelp_Click
-            AddHandler BtnGoBack.Click, AddressOf BtnGoBack_Click
-        End Sub
+''' <summary>
+''' Loads the version info section from the global config.
+''' </summary>
+Private Sub LoadAboutInfo()
+    Try
+    Dim jsonText As String = File.ReadAllText(configFilePath)
+    Dim doc As JsonDocument = JsonDocument.Parse(jsonText)
+    Dim versionElement As JsonElement = doc.RootElement.GetProperty("version")
+    TxtCreatedBy.Text = versionElement.GetProperty("created_by").GetString()
+    TxtSupportContact.Text = versionElement.GetProperty("support_email").GetString()
+    TxtLastUpdate.Text = versionElement.GetProperty("date").GetString()
+    TxtVersion.Text = versionElement.GetProperty("number").GetString()
+    TxtLatestChange.Text = versionElement.GetProperty("instructions").GetString()
+    Catch ex As Exception
+    ' TODO test if msgboxhelper works in this context
+    ' Windows.MessageBox.Show("Failed To load About information: " & ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error)
+    MsgBoxHelper.Show("Failed to load About information:" & ex.Message)
+    End Try
+End Sub
 
-        ''' <summary>
-        ''' Loads the version info section from the global config.
-        ''' </summary>
-        Private Sub LoadAboutInfo()
-            Try
-                Dim jsonText As String = File.ReadAllText(configFilePath)
-                Dim doc As JsonDocument = JsonDocument.Parse(jsonText)
+Private Sub BtnHelp_Click(sender As Object, e As RoutedEventArgs)
+    ' Windows.MessageBox.Show("Help file Not yet available.", "Coming Soon", MessageBoxButton.OK, MessageBoxImage.Information)
+    MsgBoxHelper.Show("Help file Not yet available")
+End Sub
 
-                Dim versionElement As JsonElement = doc.RootElement.GetProperty("version")
-
-                TxtCreatedBy.Text = versionElement.GetProperty("created_by").GetString()
-                TxtSupportContact.Text = versionElement.GetProperty("support_email").GetString()
-                TxtLastUpdate.Text = versionElement.GetProperty("date").GetString()
-                TxtVersion.Text = versionElement.GetProperty("number").GetString()
-                TxtLatestChange.Text = versionElement.GetProperty("instructions").GetString()
-
-            Catch ex As Exception
-                Windows.MessageBox.Show("Failed to load About information: " & ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error)
-            End Try
-        End Sub
-
-        Private Sub BtnHelp_Click(sender As Object, e As RoutedEventArgs)
-            Windows.MessageBox.Show("Help file not yet available.", "Coming Soon", MessageBoxButton.OK, MessageBoxImage.Information)
-        End Sub
-
-        Private Sub BtnGoBack_Click(sender As Object, e As RoutedEventArgs)
-            _hostForm.Close()
-        End Sub
-    End Class
+Private Sub BtnGoBack_Click(sender As Object, e As RoutedEventArgs)
+    _hostForm.Close()
+End Sub
+End Class
 
 End Namespace
