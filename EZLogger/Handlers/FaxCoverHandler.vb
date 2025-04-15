a31687e0ab5ebef7a35c1ae2e49d5f17510ae8f9
lundeen-bryan <lundeen.bryan@gmail.com>
2025-04-15 06:19:34 -0700

    Add background color and new handler classes
    Updated `ChiefApprovalView.xaml` and `FaxCoverView.xaml` to include a `Background="White"` attribute for improved visual consistency. Introduced `FaxCoverHandler` and `SaveFileHandler` classes in their respective files, providing methods to display fax cover and save file host windows.

---
+++ EZLogger/Handlers/FaxCoverHandler.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Windows
+Imports System.Windows.Forms
+
+Namespace Handlers
+
+    ''' <summary>
+    ''' Provides logic related to generating or managing fax cover sheets in EZLogger.
+    ''' </summary>
+    Public Class FaxCoverHandler
+
+        ''' <summary>
+        ''' Displays a placeholder message box for fax cover logic.
+        ''' </summary>
+        Public Sub ShowFaxCoverMessage()
+            Dim host As New FaxCoverHost()
+            host.TopMost = True
+            FormPositionHelper.MoveFormToTopLeftOfAllScreens(host, 10, 10)
+            host.Show()
+        End Sub
+
+    End Class
+
+End Namespace
---
+++ EZLogger/Handlers/SaveFileHandler.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Windows
+Imports System.Windows.Forms
+
+Namespace Handlers
+
+    ''' <summary>
+    ''' Provides logic for save-related operations in EZLogger.
+    ''' </summary>
+    Public Class SaveFileHandler
+
+        ''' <summary>
+        ''' Displays a message box as a placeholder for save file logic.
+        ''' </summary>
+        Public Sub ShowSaveMessage()
+            Dim host As New SaveFileHost()
+            host.TopMost = True
+            FormPositionHelper.MoveFormToTopLeftOfAllScreens(host, 10, 10)
+            host.Show()
+        End Sub
+
+    End Class
+
+End Namespace
