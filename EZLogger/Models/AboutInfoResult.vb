72b91055e1d8ae779231c74bb7ed331a329e360f
lundeen-bryan <lundeen.bryan@gmail.com>
2025-04-20 07:10:01 -0700

    Add AboutWinHandler and refactor AboutView logic
    Updated `EZLogger.vbproj` to include new files for handling about information. Refactored `AboutView.xaml.vb` to utilize `AboutWinHandler` for loading version metadata, improving separation of concerns. Introduced `AboutInfoResult` model to encapsulate version information and error handling. Overall, these changes enhance code organization, maintainability, and readability.

---
+++ EZLogger/Models/AboutInfoResult.vb
@@ -0,0 +1,50 @@
+﻿Namespace Models
+
+    ''' <summary>
+    ''' Represents version information loaded from the global_config.json file.
+    ''' Used by the AboutViewHandler to pass metadata to the AboutView.
+    ''' </summary>
+    Public Class AboutInfoResult
+
+        ''' <summary>
+        ''' The name of the person who created this version of the application.
+        ''' </summary>
+        Public Property CreatedBy As String
+
+        ''' <summary>
+        ''' The support contact email address.
+        ''' </summary>
+        Public Property SupportEmail As String
+
+        ''' <summary>
+        ''' The date of the most recent update.
+        ''' </summary>
+        Public Property LastUpdate As String
+
+        ''' <summary>
+        ''' The version number of the application.
+        ''' </summary>
+        Public Property VersionNumber As String
+
+        ''' <summary>
+        ''' A short description of the latest change or instruction.
+        ''' </summary>
+        Public Property LatestChange As String
+
+        ''' <summary>
+        ''' If loading fails, this contains the error message to be displayed.
+        ''' </summary>
+        Public Property ErrorMessage As String
+
+        ''' <summary>
+        ''' Returns True if an error occurred while loading version info.
+        ''' </summary>
+        Public ReadOnly Property HasError As Boolean
+            Get
+                Return Not String.IsNullOrEmpty(ErrorMessage)
+            End Get
+        End Property
+
+    End Class
+
+End Namespace
