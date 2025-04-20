e3e88792c58d77dcc88fbfe6656aef8867aba149
lundeen-bryan <lundeen.bryan@gmail.com>
2025-04-19 21:42:19 -0700

    Add trash icon and update ribbon functionality
    - Introduced `trash_icon.png` resource to the project.
    - Enhanced `EZLoggerRibbon.Designer.vb` with new buttons and groups, including `DeleteDocPropsBtn`, `PdfBtnBox`, and `TypoBtnBox`.
    - Implemented event handler for `DeleteDocPropsBtn` to clear custom document properties.
    - Updated `My.Resources` to include `trash_icon` and modified `Resources.resx` for new icons.
    - Managed resource references by removing and re-adding the `cog` resource.

--- EZLogger/My Project/Resources.Designer.vb
+++ EZLogger/My Project/Resources.Designer.vb
@@ -253,6 +253,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property trash_icon() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("trash_icon", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property typo() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("typo", resourceCulture)
