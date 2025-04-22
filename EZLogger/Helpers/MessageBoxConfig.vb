de485f00edaf8c1eedb35fce18ac410a8c9c22b6
Lundeen.Bryan <lundeen.bryan@gmail.com>
2025-04-22 11:00:20 -0700

    Added summary to MessageBoxConfig

--- EZLogger/Helpers/MessageBoxConfig.vb
+++ EZLogger/Helpers/MessageBoxConfig.vb
@@ -1,4 +1,12 @@
-﻿Public Class MessageBoxConfig
+﻿'''<summary>
+'''Defines the configuration options for displaying custom message boxes
+'''in EZLogger. This class is used by MsgBoxHelper to control which
+'''buttons (Yes, No, Ok) should be shown and what message should be
+'''displayed. This file is located in the Helpers folder because it
+'''supports helper functionality and is not an Enum or standalone UI
+'''component.
+'''</summary>
+Public Class MessageBoxConfig
     Public Property Message As String
     Public Property ShowYes As Boolean = False
     Public Property ShowNo As Boolean = False
