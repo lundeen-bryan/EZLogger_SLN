98504c4cac60a0663d2a56394cd5d7a7bacac991
Lundeen.Bryan <lundeen.bryan@gmail.com>
2025-04-23 13:00:46 -0700

    fix(handler pointers): Fixed code that point to the hanlder for each sub procedure

--- EZLogger/Views/SaveFileView.xaml.vb
+++ EZLogger/Views/SaveFileView.xaml.vb
@@ -1,4 +1,8 @@
-﻿Imports System.Windows
+﻿' Namespace=EZLogger/Views
+' Filename=SaveFileView.xaml.vb
+' !See Label Footer for notes
+
+Imports System.Windows
 Imports EZLogger.Helpers
 Imports EZLogger.Handlers
 Imports System.Windows.Forms
@@ -17,21 +21,39 @@ Public Class SaveFileView
     End Sub
 
     Private Sub WireUpButtons()
-        AddHandler BtnDone.Click, AddressOf Btn_Close_Click
+        AddHandler Me.Loaded, AddressOf SaveFileView_Loaded
+        AddHandler DoneBtn.Click, AddressOf DoneBtn_Click
         AddHandler BtnSearchPatientId.Click, AddressOf BtnSearchPatientId_Click
     End Sub
-    Private Sub BtnSearchPatientId_Click(sender As Object, e As RoutedEventArgs) Handles BtnSearchPatientId.Click
+    Private Sub BtnSearchPatientId_Click(sender As Object, e As RoutedEventArgs)
         _handler.HandleSearchPatientIdClick(Me)
     End Sub
 
-    Private Sub SaveFileView_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+    Private Sub SaveFileView_Loaded(sender As Object, e As RoutedEventArgs)
         Dim reportTypes As List(Of String) = rthandler.GetReportTypes()
         ReportTypeCbo.ItemsSource = reportTypes
     End Sub
-    Private Sub Btn_Close_Click(sender As Object, e As RoutedEventArgs)
+
+    Private Sub DoneBtn_Click(sender As Object, e As RoutedEventArgs)
         Dim panel = TaskPaneHelper.GetTaskPane()
         panel?.MarkCheckboxAsDone("Btn_I")
         _handler.HandleCloseClick(_hostForm)
     End Sub
 
 End Class
+
+' Footer:
+''===========================================================================================
+'' Procedure: ......... SaveFileView.xaml.vb/
+'' Description: ....... Saves copies of files with proper naming convention and deletes old
+'' Version: ........... 1.0.0 - major.minor.patch
+'' Created: ........... 2025-04-23
+'' Updated: ........... 2025-04-23
+'' Module URL: ........ weburl
+'' Installs to: ....... EZLogger/Views
+'' Compatibility: ..... Word VSTO
+'' Contact Author: .... lundeen-bryan
+'' Copyright:  ........ n/a ©2025. All rights reserved.
+'' Notes: ............. _
+'  (1) notes_here
+''===========================================================================================
