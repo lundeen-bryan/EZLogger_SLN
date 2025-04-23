91b69de6198c63acd37d0d8e6a1a557fb9a50080
Lundeen.Bryan <lundeen.bryan@gmail.com>
2025-04-23 13:27:56 -0700

    Removed redundant code in PatientInfoView.xaml.vb

--- EZLogger/Views/PatientInfoView.xaml.vb
+++ EZLogger/Views/PatientInfoView.xaml.vb
@@ -5,12 +5,10 @@ Imports EZLogger.Handlers
 Namespace EZLogger.Views
 
 	Partial Public Class PatientInfoView
-		Inherits System.Windows.Controls.UserControl
+		Inherits Controls.UserControl
 
 		Private ReadOnly _handler As PatientInfoHandler
 		Private ReadOnly _hostForm As New Form()
-
-		Public Event CloseButtonClicked(sender As Object, e As EventArgs)
 
 		Public Sub New(Optional hostForm As Form = Nothing)
 			InitializeComponent()
@@ -45,9 +43,11 @@ Namespace EZLogger.Views
 		Private Sub BtnDeleteAll_Click(sender As Object, e As RoutedEventArgs)
 			_handler.HandleDeleteAllClick(Me)
 		End Sub
+
 		Private Sub BtnRefresh_Click(sender As Object, e As RoutedEventArgs)
 			_handler.LoadCustomDocProperties(Me)
 		End Sub
+
 		Private Sub BtnFirstPage_Click(sender As Object, e As RoutedEventArgs)
 			_handler.HandleFirstPageClick()
 		End Sub
@@ -55,6 +55,7 @@ Namespace EZLogger.Views
 		Private Sub BtnLastPage_Click(sender As Object, e As RoutedEventArgs)
 			_handler.HandleLastPageClick()
 		End Sub
+
 		Private Sub BtnAddEdit_Click(sender As Object, e As RoutedEventArgs)
 			_handler.HandleAddEditClick(Me)
 		End Sub
