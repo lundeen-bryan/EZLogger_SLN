8eb3aeed02eb250c754ebb2755933dc0b9225363
lundeen-bryan <lundeen.bryan@gmail.com>
2025-04-28 09:33:24 -0700

    Add drag-and-drop functionality to TaskListView
    This update introduces private fields `_startPoint` and `_draggedItem` to manage drag operations. The `OnPreviewMouseLeftButtonDown` method captures the initial mouse position and identifies the clicked row, while the `OnMouseMove` method initiates the drag-and-drop action if the mouse is moved sufficiently. These enhancements improve user interaction by allowing items to be dragged within the `TaskListView`.

--- EZLogger/Views/TaskListView.xaml.vb
+++ EZLogger/Views/TaskListView.xaml.vb
@@ -1,9 +1,15 @@
-﻿Imports UserControl = System.Windows.Controls.UserControl
+﻿Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports UserControl = System.Windows.Controls.UserControl
 
 Partial Public Class TaskListView
     Inherits UserControl
 
     Private _handler As TaskListHandler
+    Private _startPoint As Point
+    Private _draggedItem As TaskItem
 
     Public Sub New()
         InitializeComponent()
@@ -19,12 +25,37 @@ Partial Public Class TaskListView
         _handler.RemoveCompletedTasks()
     End Sub
 
-    Private Sub OnPreviewMouseLeftButtonDown(sender As Object, e As System.Windows.Input.MouseButtonEventArgs)
-        ' Will add code later
+    Private Sub OnPreviewMouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs)
+        _startPoint = e.GetPosition(Nothing)
+
+        ' Try to find the clicked row
+        Dim element = CType(e.OriginalSource, DependencyObject)
+        While element IsNot Nothing AndAlso Not TypeOf element Is DataGridRow
+            element = VisualTreeHelper.GetParent(element)
+        End While
+
+        If element IsNot Nothing Then
+            Dim row = CType(element, DataGridRow)
+            _draggedItem = CType(row.Item, TaskItem)
+        Else
+            _draggedItem = Nothing
+        End If
     End Sub
 
-    Private Sub OnMouseMove(sender As Object, e As System.Windows.Input.MouseEventArgs)
-        ' Will add code later
+    Private Sub OnMouseMove(sender As Object, e As MouseEventArgs)
+        If e.LeftButton = MouseButtonState.Pressed AndAlso _draggedItem IsNot Nothing Then
+            Dim currentPosition = e.GetPosition(Nothing)
+            Dim diff = _startPoint - currentPosition
+
+            ' Check if moved enough distance to start drag
+            If Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance OrElse
+           Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance Then
+
+                ' Begin drag-drop operation
+                Dim data = New DataObject(GetType(TaskItem), _draggedItem)
+                DragDrop.DoDragDrop(TasksDataGrid, data, DragDropEffects.Move)
+            End If
+        End If
     End Sub
 
     Private Sub OnDrop(sender As Object, e As System.Windows.DragEventArgs)
