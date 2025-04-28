183dd317c5aba7dce1677a02879051d37ea6cc08
lundeen-bryan <lundeen.bryan@gmail.com>
2025-04-27 21:24:36 -0700

    Add TaskCollection class for XML serialization support
    Updated `EZLogger.vbproj` to include `TaskCollection.vb`.
    Introduced `TaskCollection` class with XML serialization,
    containing a list of `TaskItem` objects initialized in the
    constructor and marked with the `XmlRoot` attribute.

---
+++ EZLogger/Models/TaskCollection.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Xml.Serialization
+
+<XmlRoot("Tasks")>
+Public Class TaskCollection
+
+    <XmlElement("Task")>
+    Public Property Items As List(Of TaskItem)
+
+    Public Sub New()
+        Items = New List(Of TaskItem)()
+    End Sub
+
+End Class
