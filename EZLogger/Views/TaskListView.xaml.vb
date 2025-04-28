88a7bf49dab1f8cc6fb12a402ed08eee598cec58
lundeen-bryan <lundeen.bryan@gmail.com>
2025-04-27 22:18:55 -0700

    Refactor TaskListView to inherit from UserControl
    Updated the TaskListView class to inherit from System.Windows.Controls.UserControl, enabling it to function as a WPF user control. Added an Imports statement for UserControl and implemented a constructor that calls InitializeComponent() to initialize the control's components.

--- EZLogger/Views/TaskListView.xaml.vb
+++ EZLogger/Views/TaskListView.xaml.vb
@@ -1,3 +1,10 @@
-﻿Public Class TaskListView
+﻿Imports UserControl = System.Windows.Controls.UserControl
+
+Partial Public Class TaskListView
+    Inherits UserControl
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
 
 End Class
