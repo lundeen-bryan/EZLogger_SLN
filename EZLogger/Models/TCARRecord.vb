e21bb3e988f29d8ef96d0dfce4b863cad6bf497f
lundeen-bryan <lundeen.bryan@gmail.com>
2025-05-08 07:17:03 -0700

    Update TCAR data handling and structure
    Modified SQL query in TCARListHandler.vb to select data from EZL_TcarView. Updated TCARRecord properties to reflect new data structure, removing OpID and adding AssignedTo. Commented out OpinionDescription property to reflect changes in functionality.

--- EZLogger/Models/TCARRecord.vb
+++ EZLogger/Models/TCARRecord.vb
@@ -3,16 +3,16 @@
     Public Property Tcar As Integer
     Public Property PatientName As String
     Public Property Subdate As String
-    Public Property OpID As Integer
+    Public Property AssignedTo As String
 
-    Public ReadOnly Property OpinionDescription As String
-        Get
-            Select Case OpID
-                Case 1 : Return "Unlikely"
-                Case 2 : Return "Competent"
-                Case 3 : Return "Not Yet Competent"
-                Case Else : Return "Unknown"
-            End Select
-        End Get
-    End Property
+    'Public ReadOnly Property OpinionDescription As String
+    '    Get
+    '        Select Case OpID
+    '            Case 1 : Return "Unlikely"
+    '            Case 2 : Return "Competent"
+    '            Case 3 : Return "Not Yet Competent"
+    '            Case Else : Return "Unknown"
+    '        End Select
+    '    End Get
+    'End Property
 End Class
